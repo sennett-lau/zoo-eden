60a4f718d08508f555f3fff5e2df7708f7a45282
Sennett Lau <laub1199@gmail.com>
2023-09-08 21:00:02 +0800

    added food properties for tangerine

--- src/main/java/net/sennettlau/zooedenmod/item/ModItems.java
+++ src/main/java/net/sennettlau/zooedenmod/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.sennettlau.zooedenmod.item;
 
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -8,11 +9,16 @@ import net.minecraftforge.registries.RegistryObject;
 import net.sennettlau.zooedenmod.ZooEdenMod;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, ZooEdenMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ZooEdenMod.MOD_ID);
 
-    public static final RegistryObject<Item> TANGERINE = ITEMS.register("tangerine",
-            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TANGERINE = ITEMS.register(
+            "tangerine",
+            () -> new Item(
+                    new Item.Properties().stacksTo(64).food(
+                            new FoodProperties.Builder().nutrition(2).saturationMod(0.3f).build()
+                    )
+            )
+    );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
